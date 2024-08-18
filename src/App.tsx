1fbdc411198d61fc68dc3e6bf5aa64ab331f8e45
Jack Hsu <contactjackhsu@gmail.com>
2024-08-18 18:12:01 +0800

    remove logo

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,6 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
       </div>
       <h1>hello</h1>
       <div className="card">
