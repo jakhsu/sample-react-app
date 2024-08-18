fde2f88d68e5167605a28c60dc8e2e1d283cd33b
Jack Hsu <contactjackhsu@gmail.com>
2024-08-18 15:34:13 +0800

    Update button text in App component

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           test 2 count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Push <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
