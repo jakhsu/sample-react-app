051db79dc1315e32f151fc5ca22a95d4cd86f4e8
Jack Hsu <contactjackhsu@gmail.com>
2024-08-18 15:21:26 +0800

    refactor: update button text in App component

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Test</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          test count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
