0d5ebc362a5e72f9ae5af0c025c703f38c3784af
Jack Hsu <contactjackhsu@gmail.com>
2024-08-25 15:05:01 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       <h1>hi</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          test 3 count are {count}
+          test 4 count are {count}
         </button>
         <p>
           change <code>src/App.tsx</code> and try again to save to test hot module replacement.
